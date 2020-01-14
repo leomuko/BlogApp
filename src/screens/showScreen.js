0544c29def332ee76bab38d496acb54eddc58ad3
leomuko <kezzieleo@gmail.com>
2020-01-14 10:06:57 +0300

    Create Show Screen

---
+++ src/screens/showScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const ShowScreen = () =>{
+    return (
+        <View>
+            <Text>Show Screen</Text>
+        </View>
+    )
+
+}
+
+const Styles = StyleSheet.create({});
+
+export default ShowScreen;
