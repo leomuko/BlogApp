d0f08f04cfae91d3bf7f938217b317958813a062
leomuko <kezzieleo@gmail.com>
2020-01-14 08:59:14 +0300

    Modify Screen

--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react';
-import { View, Text, FlatList, Button } from 'react-native';
+import { View, Text, FlatList, Button, StyleSheet } from 'react-native';
 import {Context} from '../context/blogContext';
+import {Feather} from '@expo/vector-icons';
 
 const IndexScreen = () => {
     const {state, addBlogPost} = useContext(Context);
 
     return (
         <View>
-            <Text>Screen</Text>
+            
             <Button
             title= "Add Post"
             onPress={addBlogPost}
@@ -16,13 +17,31 @@ const IndexScreen = () => {
                 data={state}
                 keyExtractor={blogPost => blogPost.title}
                 renderItem={({item}) => {
-                    return <Text>{item.title}</Text>
-                    
+                    return <View style={styles.row}>
+                    <Text style={styles.title}>{item.title}</Text>
+                    <Feather name="trash" style={styles.icon}/>
+                    </View>
                 }
                 }
             />
         </View>
     )
 }
+const styles = StyleSheet.create({
+    row:{
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        paddingVertical: 20,
+        borderTopWidth: 1,
+        borderColor :'gray',
+        paddingHorizontal: 10
+    },
+    title:{
+        fontSize: 18
+    },
+    icon:{
+        fontSize: 20
+    }
+});
 
 export default IndexScreen;
