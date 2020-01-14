6760c342d08c0f86b93a3a66e67078405a0ac030
leomuko <kezzieleo@gmail.com>
2020-01-14 10:49:09 +0300

    Add Creation Screen

--- App.js
+++ App.js
@@ -4,10 +4,12 @@ import IndexScreen from './src/screens/indexScreen';
 import React from 'react';
 import {Provider} from './src/context/blogContext';
 import ShowScreen from './src/screens/showScreen';
+import CreateScreen from './src/screens/createScreen';
 
 const navigator = createStackNavigator({
   Index: IndexScreen,
-  Show: ShowScreen
+  Show: ShowScreen,
+  Create: CreateScreen
 }, {
   initialRouteName: 'Index',
   defaultNavigationOptions: {
---
+++ src/screens/createScreen.js
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+
+const CreateScreen = () => {
+    
+
+    return (
+        <View>
+            <Text>Creat Blog Post </Text>
+        </View>
+    )
+
+}
+
+const Styles = StyleSheet.create({});
+
+export default CreateScreen;
