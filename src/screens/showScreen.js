f47b9c99c70d45d8f7ea4748c1ca6f0499c5b3d0
leomuko <kezzieleo@gmail.com>
2020-01-14 10:27:10 +0300

    Retrieving a single post

--- src/screens/showScreen.js
+++ src/screens/showScreen.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import React, { useContext } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Context } from '../context/blogContext';
 
-const ShowScreen = () =>{
+const ShowScreen = ({ navigation }) => {
+    const { state } = useContext(Context);
+    const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'))
+
     return (
         <View>
-            <Text>Show Screen</Text>
+            <Text>{blogPost.title}</Text>
         </View>
     )
 
