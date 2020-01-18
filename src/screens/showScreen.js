a664ebfaf4724eabead72911362534b1b1c89b10
leomuko <kezzieleo@gmail.com>
2020-01-18 16:27:17 +0300

    Add edit icon

--- src/screens/showScreen.js
+++ src/screens/showScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { Context } from '../context/blogContext';
+import {EvilIcons} from '@expo/vector-icons';
 
 const ShowScreen = ({ navigation }) => {
     const { state } = useContext(Context);
@@ -9,9 +10,17 @@ const ShowScreen = ({ navigation }) => {
     return (
         <View>
             <Text>{blogPost.title}</Text>
+            <Text>{blogPost.content}</Text>
         </View>
     )
 
+}
+ShowScreen.navigationOptions = ({navigation}) =>{
+    return{
+        headerRight: <TouchableOpacity onPress={() => navigation.navigate('Edit')}>
+            <EvilIcons name="pencil" size={40} />
+        </TouchableOpacity>
+    };
 }
 
 const Styles = StyleSheet.create({});
